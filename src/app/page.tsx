0a21efe34b9af7e3a62816b42512649898a41102
christapia81 <tpoei.00x1f@gmail.com>
2024-12-06 04:30:17 +0800

    add auth demo label

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Page() {
 
     <h1 className="p-4 text-3xl font-mono bg-blue-300 shadow-sm">Hello, Next.js! With <span className="text-amber-400 font-sans">Google</span> APIs</h1>
     <div className="m-7 p-4 rounded-md shadow-md">
+       Auth Demo
       <LoginButton></LoginButton>
     </div>
   </>
