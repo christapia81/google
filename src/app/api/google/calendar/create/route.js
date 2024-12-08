fca9e5df49900eb842cd4071846d3ad7de9f1b8e
christapia81 <tpoei.00x1f@gmail.com>
2024-12-09 03:32:13 +0800

    exchange code for tokens

--- src/app/api/google/calendar/create/route.js
+++ src/app/api/google/calendar/create/route.js
@@ -3,5 +3,18 @@ import { google } from 'googleapis'
 
 export async function POST(request) {
   const res = await request.json()
-  return Response.json({ res })
+  //expect res as { code: "xyz"}
+  const code = res.code;
+
+  const oAuth2Client = new google.auth.OAuth2(
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+    'postmessage'
+  );
+
+  const { tokens } = await oAuth2Client.getToken(code);
+
+
+
+  return Response.json({ tokens })
 }
