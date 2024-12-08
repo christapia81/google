1a83e6d15f57661da3eeb9e47d3e64688a18abcd
christapia81 <tpoei.00x1f@gmail.com>
2024-12-09 04:34:54 +0800

    trap error on calender create requests

--- src/app/api/google/calendar/create/route.js
+++ src/app/api/google/calendar/create/route.js
@@ -2,45 +2,46 @@
 import { google } from 'googleapis'
 
 export async function POST(request) {
-  const res = await request.json()
-  //expect res as { code: "xyz"}
-
-
-  const { code, summary, location, description , start_date, end_date, timezone} = res
-
-  const oAuth2Client = new google.auth.OAuth2(
-    process.env.GOOGLE_CLIENT_ID,
-    process.env.GOOGLE_CLIENT_SECRET,
-    'postmessage'
-  );
-
-  const { tokens } = await oAuth2Client.getToken(code);
-
-  const { access_token, refresh_token } = tokens;
-
-  oAuth2Client.setCredentials({ access_token, refresh_token });
-
-  const calendar = google.calendar({
-    version: 'v3',
-    auth: oAuth2Client
-  });
-
-  const event = {
-    summary: summary,
-    location: location,
-
-    description: description,
-    start: {
-      dateTime: start_date/* "2024-12-14T19:30:00+05:30" */,
-      timeZone: timezone
-    },
-    end: {
-      dateTime: end_date/* "2024-12-14T20:30:00+05:30" */,
-      timeZone: timezone
-    },
-  };
-
   try {
+    const res = await request.json()
+    //expect res as { code: "xyz"}
+
+
+    const { code, summary, location, description, start_date, end_date, timezone } = res
+
+    const oAuth2Client = new google.auth.OAuth2(
+      process.env.GOOGLE_CLIENT_ID,
+      process.env.GOOGLE_CLIENT_SECRET,
+      'postmessage'
+    );
+
+    const { tokens } = await oAuth2Client.getToken(code);
+
+    const { access_token, refresh_token } = tokens;
+
+    oAuth2Client.setCredentials({ access_token, refresh_token });
+
+    const calendar = google.calendar({
+      version: 'v3',
+      auth: oAuth2Client
+    });
+
+    const event = {
+      summary: summary,
+      location: location,
+
+      description: description,
+      start: {
+        dateTime: start_date/* "2024-12-14T19:30:00+05:30" */,
+        timeZone: timezone
+      },
+      end: {
+        dateTime: end_date/* "2024-12-14T20:30:00+05:30" */,
+        timeZone: timezone
+      },
+    };
+
+
     const result = await calendar.events.insert({
       calendarId: 'primary',
       auth: oAuth2Client,
@@ -48,7 +49,7 @@ export async function POST(request) {
     });
 
 
-    return Response.json({ message: 'Event created', result })
+    return Response.json({ message: 'Event created'/* , data: result  */ })
   } catch (err) {
     console.log(err);
     return Response.json({ err })
